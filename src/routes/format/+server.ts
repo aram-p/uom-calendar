3a8adf4032a1239ebe6e36a0b47e14c3b922c650
Aram <arampoghossian@gmail.com>
2023-02-24 15:36:07 +0000

    add link to checkin

--- src/routes/format/+server.ts
+++ src/routes/format/+server.ts
@@ -4,39 +4,7 @@ import type { RequestHandler } from "./$types";
 import { createEvents } from "ics";
 import type { EventAttributes, DateArray } from "ics";
 
-const courses = [
-  { name: "Virtual Common Room", code: "EEEN20000" },
-  { name: "Microcontroller Engineering II", code: "EEEN20011" },
-  { name: "Engineering Management", code: "EEEN20051" },
-  { name: "Digital Systems Design II", code: "EEEN20121" },
-  { name: "Signals and Systems", code: "EEEN20131" },
-  { name: "Machines, Drives & Power Electronics", code: "EEEN20212" },
-  { name: "Electronic Circuit Design II", code: "EEEN20222" },
-  { name: "Generation and Transport of Electrical Energy", code: "EEEN20242" },
-  { name: "Control Systems I", code: "EEEN20252" },
-  { name: "Analogue and Digital Communications", code: "EEEN20262" },
-  { name: "Embedded Systems Project", code: "EEEN21000" },
-  { name: "Aspire - Your Future", code: "EEEN29040" },
-  { name: "Mathematics 2E1", code: "MATH29681" },
-  { name: "Economic Principles", code: "BMAN10001" },
-  { name: "Marketing Foundations", code: "BMAN10101" },
-  { name: "Financial Decision Making M", code: "BMAN10522M" },
-  { name: "Fundamentals of Financial Reporting", code: "BMAN10621M" },
-  { name: "Fundamentals of Management Accounting M", code: "BMAN10632M" },
-  { name: "Academic and Career Development", code: "BMAN10780" },
-  { name: "Case Studies in Professional Management", code: "BMAN10862" },
-  { name: "Introduction to Work Psychology", code: "BMAN10872" },
-  {
-    name: "Quantitative Methods for Business and Management",
-    code: "BMAN10960",
-  },
-  {
-    name: "Introduction to Management and Organisation Studies",
-    code: "BMAN10970",
-  },
-  { name: "Academic Malpractice Awareness", code: "BMAN11021" },
-  { name: "Health & Safety Induction", code: "BMAN11230" },
-] as const;
+import { courses } from "./data";
 
 function parseDate(date: Date): DateArray {
   const year = date.getFullYear();
@@ -79,6 +47,8 @@ export const GET: RequestHandler = async ({ url, setHeaders }) => {
       }
     );
 
+    const checkInLink = "https://my.manchester.ac.uk/MyCheckIn";
+
     const ics_events = events
       .map((event) => {
         if (event.type === "VEVENT")
@@ -87,7 +57,7 @@ export const GET: RequestHandler = async ({ url, setHeaders }) => {
             start: parseDate(event.start),
             end: parseDate(event.end),
             title: event.summary,
-            description: event.description,
+            description: `${event.description}\n\n${checkInLink}`,
             location: event.location,
           };
         return undefined;
